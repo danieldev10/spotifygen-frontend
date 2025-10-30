691ef5c3cbd1a6d3a7d4bb5fc866409afa7607ff
Achonma Daniel Tanko <danielachonma10@gmail.com>
2025-10-30 17:02:09 +0100

    Fix: corrected import path case for Register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
-import Register from './pages/register';
+import Register from './pages/Register';
 import Login from './pages/Login';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
