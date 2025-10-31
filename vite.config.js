f3ba2e6e4695f33633814a4ebce0e7fa5430bc73
Achonma Daniel Tanko <danielachonma10@gmail.com>
2025-10-31 11:50:53 +0100

    static fixing 4

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,11 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true
+  },
+  preview: {
+    historyApiFallback: true
   }
 })
