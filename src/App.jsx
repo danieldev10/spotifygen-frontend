94f8a8a2adcc75ce9817753cc24310b47c98644f
Achonma Daniel Tanko <danielachonma10@gmail.com>
2025-10-31 12:18:49 +0100

    static fixing 7

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/home' element={<Home />}></Route>
+        <Route path='/' element={<Home />}></Route>
         <Route path='/register' element={<Register />}></Route>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/forgot-password' element={<ForgotPassword />}></Route>
